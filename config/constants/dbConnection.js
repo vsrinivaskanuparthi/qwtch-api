0cb7c747cd512baef7c9a0ac38ae882c1c72e5e8
srinivas kanuparthi <srinivas.kanuparthi@mdc.anblicks.com>
2019-11-01 00:16:31 +0530

    updated changes

--- config/constants/dbConnection.js
+++ config/constants/dbConnection.js
@@ -9,17 +9,21 @@ var dbConnectionObject = undefined
 module.exports.initDBConnections = function () {
     if (!dbConnectionObject) {
         dbConnectionObject = mysql.createConnection({
-            host: process.env.DB_HOST || 'database-2.cw6isne4xqja.us-east-1.rds.amazonaws.com',
-            user: process.env.DB_USER || 'aeroadmin',
-            password: process.env.DB_PASSWORD || 'aer0Admin',
-            database: process.env.DB_NAME || 'qwtchcom_qwatchdb',
+            host: process.env.DB_HOST || 'qwtchcom-dev.cib6odvvsdkd.us-east-1.rds.amazonaws.com',
+            user: process.env.DB_USER || 'qwtchcom',
+            password: process.env.DB_PASSWORD || 'qwtchcom',
+            database: process.env.DB_NAME || 'qwtch_qwtchdb',
             port: process.env.DB_PORT || 3306
         });
 
         //connect to database
         dbConnectionObject.connect((err) => {
-            if (err) throw err;
-            logger.info('Mysql Database Connection Established Successfully...');
+            if (err) {
+                logger.error('Mysql Database Connection Error Occured')
+                throw err;
+            } else {
+                logger.info('Mysql Database Connection Established Successfully...');
+            }
         });
 
     }
